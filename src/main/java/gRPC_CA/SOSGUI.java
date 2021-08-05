8922d88d153bb930486b1323ee4095abe655d622
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2021-08-05 22:14:17 +0100

    New CA file Gridle

---
+++ src/main/java/gRPC_CA/SOSGUI.java
@@ -0,0 +1,34 @@
+package gRPC_CA;
+
+import javax.swing.*;
+
+public class SOSGUI extends JFrame {
+    private JPanel SOSPanel;
+    private JButton helpMeButton;
+    private JComboBox locationOptions;
+    private JComboBox typeServices;
+    private JRadioButton areYouHurtSituation;
+    private JRadioButton areYouAloneSituation;
+    private JButton closeAppButton;
+    private JButton situationCall;
+    private JButton typeServiceCall;
+    private JButton locationCall;
+    private JPanel LeftPanelMargin;
+    private JPanel RightPanelMargin;
+
+    // Constructor
+    public SOSGUI(String title){
+        super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(SOSPanel);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new SOSGUI("SOSapp");
+        frame.setVisible(true);
+
+    }
+
+}
