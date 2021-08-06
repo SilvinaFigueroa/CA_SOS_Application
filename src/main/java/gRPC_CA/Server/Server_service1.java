db82181f8c14e911e308783cd17dc349870852c5
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2021-08-06 22:54:40 +0100

    GUI implementation with the first service

--- src/main/java/gRPC_CA/Server/Server_service1.java
+++ src/main/java/gRPC_CA/Server/Server_service1.java
@@ -2,16 +2,19 @@ package gRPC_CA.Server;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
-
 import java.io.IOException;
 
 public class Server_service1 {
     public static void main(String[] args) throws IOException, InterruptedException { // IOException for server start and awaitTermination
         System.out.println("Grpc Server setup");
 
-        Server server = ServerBuilder.forPort(50051)
+        //Build the server
+        Server server = ServerBuilder.forPort(50052)
+                //adding the server implementation
+                .addService(new Service_1impl())
                 .build();
 
+        //Start the server
         server.start();
         System.out.println("Server running");
 
--- src/main/java/gRPC_CA/Server/Service_1impl.java
+++ src/main/java/gRPC_CA/Server/Service_1impl.java
@@ -3,19 +3,23 @@ package gRPC_CA.Server;
 import com.proto.CA_Service_1.HelpMessageRequest;
 import com.proto.CA_Service_1.HelpMessageResponse;
 import com.proto.CA_Service_1.HelpMessageServiceGrpc;
-import gRPC_CA.SOSGUI;
 import io.grpc.stub.StreamObserver;
+
+import javax.swing.*;
 
 public class Service_1impl extends HelpMessageServiceGrpc.HelpMessageServiceImplBase {
 
     @Override
     public void helpMessage(HelpMessageRequest request, StreamObserver<HelpMessageResponse> responseObserver) {
 
-        SOSGUI guiapp = new SOSGUI();
-
+        //prepare the value to be set back
+        String helpRequest = request.getHelpRequest();
+        JFrame frame = new JFrame("JOptionPane showMessageDialog example");
         HelpMessageResponse helpResponse = HelpMessageResponse.newBuilder()
-                .setHelpButtonResponse(request.getHelpButtonRequest() + "").build();
+                .setHelpResponse(JOptionPane.showInputDialog("Please,follow the instructions"))
+                .build();
 
-
+        responseObserver.onNext(helpResponse);
+        responseObserver.onCompleted();
     }
 }
